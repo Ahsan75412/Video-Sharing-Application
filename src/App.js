1576fc37780fb59e2d83f7846f4aaad0ef016360
Ahsan Habib <ahsanpub16b@gmail.com>
2023-02-19 14:52:25 +0600

    added component file here!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import { Grid } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid style={{justifyContent: "center"}} container spacing={10}>
+      <Grid item xs={11}>
+          <Grid container spacing={10}>
+            <Grid item xs={12}>
+              {/* {searchBar} */}
+            </Grid>
+            <Grid item xs={8}>
+              {/* {VideoDetail} */}
+            </Grid>
+            <Grid item xs={4}>
+              {/* {VideoList} */}
+            </Grid>
+
+          </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
